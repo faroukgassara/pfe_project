7cb190caec07e9b2f316b79a2447bb1da4fae52d
faroukgassara <faroukgasaraa@gmail.com>
2020-06-07 23:09:42 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,99 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:pfe_project/login.dart';
-import 'package:pfe_project/menudashboardpage.dart';
+import 'package:http/http.dart' as http ;
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
-      home: Login(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Map data ; 
+  List usersData;
+
+  
+    
+  getUsers() async{
+    var response = await http.get(_localhost());
+    
+    data = json.decode(response.body);
+    debugPrint(response.body);
+    setState(() {
+      usersData = data["users"];
+    });
+      
+    
+  }
+
+  String _localhost() {
+    if (Platform.isAndroid)
+      return 'http://10.0.2.2:5000';
+    else // for iOS simulator
+      return 'http://localhost:5000';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+  }
+  //usersData == null ? 0 :
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter listview with json'),
+      ),
+      body: ListView.builder(
+        itemCount:  usersData.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "${usersData[index]["nom"]}",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "QSDQSD",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          );
+        },
+        
+      )
     );
   }
 }
