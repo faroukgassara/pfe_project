03e41dc2b99d600228fb373c5c1185b33c6691ce
faroukgassara <faroukgasaraa@gmail.com>
2020-06-08 22:48:04 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,154 @@
 import 'dart:convert';
-import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http ;
+import 'package:http/http.dart' as http;
 
-void main() => runApp(App());
+import 'model.dart';
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class HomePage extends StatefulWidget {
-
+class Home extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
-
-  Map data ; 
-  List usersData;
-
-  
-    
-  getUsers() async{
-    var response = await http.get('http://10.0.3.2:5000');
-    
-    data = json.decode(response.body);
-    debugPrint('response.body');
+class _HomeState extends State<Home> {
+  List<Posts> _list = [];
+  List<Posts> _search = [];
+  var loading = false;
+  Future<Null> fetchData() async {
     setState(() {
-      usersData = data["users"];
+      loading = true;
     });
-      
-    
+    _list.clear();
+    final response =
+        await http.get("http://10.0.3.2:5000");
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        for (Map i in data) {
+          _list.add(Posts.formJson(i));
+          loading = false;
+        }
+      });
+    }
   }
 
-  String _localhost() {
-    if (Platform.isAndroid)
-      return 'http://10.0.2.2:5000';
-    else // for iOS simulator
-      return 'http://localhost:5000';
+  TextEditingController controller = new TextEditingController();
+
+  onSearch(String text) async {
+    _search.clear();
+    if (text.isEmpty) {
+      setState(() {});
+      return;
+    }
+
+    _list.forEach((f) { 
+      if (f.nom.contains(text) || f.prenom.contains(text))
+        _search.add(f);
+    });
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
-    getUsers();
+    fetchData();
   }
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter listview with json'),
-      ),
-      body: ListView.builder(
-        itemCount:usersData == null ? 0 :  usersData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "${usersData[index]["nom"]}",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
-                    ),
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.blue,
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.search),
+                  title: TextField(
+                    controller: controller,
+                    onChanged: onSearch,
+                    decoration: InputDecoration(
+                        hintText: "Search", border: InputBorder.none),
                   ),
-                  Text(
-                    "QSDQSD",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
-                    ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      controller.clear();
+                      onSearch('');
+                    },
+                    icon: Icon(Icons.cancel),
                   ),
-                  
-                ],
+                ),
               ),
             ),
-          );
-        },
-        
-      )
+            loading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: _search.length != 0 || controller.text.isNotEmpty
+                        ? ListView.builder(
+                            itemCount: _search.length,
+                            itemBuilder: (context, i) {
+                              final b = _search[i];
+                              return Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        b.nom,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.0),
+                                      ),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(b.prenom),
+                                    ],
+                                  ));
+                            },
+                          )
+                        : ListView.builder(
+                            itemCount: _list.length,
+                            itemBuilder: (context, i) {
+                              final a = _list[i];
+                              return Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        a.nom,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18.0),
+                                      ),
+                                      SizedBox(
+                                        height: 4.0,
+                                      ),
+                                      Text(a.prenom),
+                                      Text(a.sex),
+                                      Text((a.statusmatriomo_id).toString()),
+                                    ],
+                                  ));
+                            },
+                          ),
+                  ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
